b9c6619abcdb1413fc3428b272c7878303e1471e
ys <ysyanshuai2@gmail.com>
2026-08-15 17:01:43 +0800

    配置修改

--- atlas-docs/docusaurus.config.ts
+++ atlas-docs/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://atlas-docs.ys0921.sbs:4443',
+  url: 'https://atlas-docs.ys0921.sbs',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
